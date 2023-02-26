e30890aba07bd26a2175e75e13ad8c95a02effb3
Alexander Nilges <github.4ug96@simplelogin.com>
2023-02-26 15:54:20 +0100

    Add Arkenfox user-overrides.js

---
+++ user-overrides.js
@@ -0,0 +1,4 @@
+// Mullvad DNS over HTTPS
+user_pref("network.trr.custom_uri", "https://doh.mullvad.net/dns-query");
+user_pref("network.trr.mode", 3);
+user_pref("network.trr.uri", "https://doh.mullvad.net/dns-query");
